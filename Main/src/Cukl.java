a250e3c68d461ccf527abe9ba738272f13929374
IgorHaidukov <kolyhool9@gmail.com>
2024-09-05 17:33:48 +0200

    new lesson

---
+++ Main/src/Cukl.java
@@ -0,0 +1,11 @@
+public class Cukl {
+    public static void main(String[] args) {
+        int i = 0;
+        for (;;){
+            if ( i >= 5)
+                break;
+            System.out.println("hello");
+            i++;
+        }
+    }
+}
